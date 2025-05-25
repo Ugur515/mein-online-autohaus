fd15a9d135b9f2621b6eea6f40a68297226ed236
Ugur <47553873+Ugur515@users.noreply.github.com>
2025-05-25 15:10:46 +0200

    github-pages einbinden

--- src/App.jsx
+++ src/App.jsx
@@ -61,98 +61,98 @@ class App
                 <div className='product-container'>
                     <Product 
                     onAdd={() => this.addItem(1, 'Audi R8', 100000)} 
-                    img="assets\img\audi_r8.jpg" 
+                    img="assets/img/audi_r8.jpg" 
                     title="Audi R8" 
                     description={"Leistung: 570 PS\nDrehmoment: 550 Nm\n0–100 km/h: 3,4 s\nHöchstgeschwindigkeit: 324 km/h\nAntrieb: Allrad\nGetriebe: 7-Gang S tronic"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Porsche 911 GT2 RS', 200000)} 
-                      img="assets\img\porsche_911_gt2rs.jpg" 
+                      img="assets/img/porsche_911_gt2rs.jpg" 
                       title="Porsche 911 GT2 RS" 
                       description={"Leistung: 700 PS\nDrehmoment: 750 Nm\n0–100 km/h: 2,8 s\nHöchstgeschwindigkeit: 340 km/h\nAntrieb: Heckantrieb\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
                         
                     <Product 
                       onAdd={() => this.addItem(1, 'Nissan R34 Skyline', 50000)} 
-                      img="assets\img\Nissan_R34_Skyline.jpg" 
+                      img="assets/img/Nissan_R34_Skyline.jpg" 
                       title="Nissan R34 Skyline" 
                       description={"Leistung: ca. 280 PS\nDrehmoment: ca. 392 Nm\n0–100 km/h: ca. 5,0 s\nHöchstgeschwindigkeit: ca. 250 km/h\nAntrieb: Allrad\nGetriebe: 5-Gang Schaltgetriebe"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Audi A5 S-Line', 45000)} 
-                      img="assets\img\audi_a5_S-line.jpg" 
+                      img="assets/img/audi_a5_S-line.jpg" 
                       title="Audi A5 S-Line" 
                       description={"Leistung: 265 PS\nDrehmoment: 370 Nm\n0–100 km/h: 5,5 s\nHöchstgeschwindigkeit: 250 km/h\nAntrieb: Allrad\nGetriebe: 7-Gang S tronic"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'BMW M2 Competition', 60000)} 
-                      img="assets\img\bmw_m2_comp.jpg" 
+                      img="assets/img/bmw_m2_comp.jpg" 
                       title="BMW M2 Competition" 
                       description={"Leistung: 410 PS\nDrehmoment: 550 Nm\n0–100 km/h: 4,2 s\nHöchstgeschwindigkeit: 280 km/h\nAntrieb: Hinterrad\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'BMW M5 F10', 40000)} 
-                      img="assets\img\BMW_M5.jpg" 
+                      img="assets/img/BMW_M5.jpg" 
                       title="BMW M5 F10" 
                       description={"Leistung: 560 PS\nDrehmoment: 680 Nm\n0–100 km/h: 4,3 s\nHöchstgeschwindigkeit: 305 km/h\nAntrieb: Hinterrad\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Corvette C7', 55000)} 
-                      img="assets\img\corvette_C7.jpg" 
+                      img="assets/img/corvette_C7.jpg" 
                       title="Corvette C7" 
                       description={"Leistung: 466 PS\nDrehmoment: 630 Nm\n0–100 km/h: 4,2 s\nHöchstgeschwindigkeit: 290 km/h\nAntrieb: Hinterrad\nGetriebe: 7-Gang manuell"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Dodge SRT Demon', 90000)} 
-                      img="assets\img\Dodge_srt_demon.jpg" 
+                      img="assets/img/Dodge_srt_demon.jpg" 
                       title="Dodge SRT Demon" 
                       description={"Leistung: 852 PS\nDrehmoment: 1044 Nm\n0–100 km/h: 2,3 s\nHöchstgeschwindigkeit: 270 km/h\nAntrieb: Hinterrad\nGetriebe: 8-Gang Automatik"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Ford GT', 400000)} 
-                      img="assets\img\ford_GT.jpg" 
+                      img="assets/img/ford_GT.jpg" 
                       title="Ford GT" 
                       description={"Leistung: 656 PS\nDrehmoment: 746 Nm\n0–100 km/h: 3,0 s\nHöchstgeschwindigkeit: 348 km/h\nAntrieb: Hinterrad\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Lamborghini Huracán SVJ', 350000)} 
-                      img="assets\img\lamborghini_huracan_svj.jpg" 
+                      img="assets/img/lamborghini_huracan_svj.jpg" 
                       title="Lamborghini Huracán SVJ" 
                       description={"Leistung: 770 PS\nDrehmoment: 720 Nm\n0–100 km/h: 2,9 s\nHöchstgeschwindigkeit: 325 km/h\nAntrieb: Allrad\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'McLaren 720S GT', 300000)} 
-                      img="assets\img\mclaren_720s_GT.jpg" 
+                      img="assets/img/mclaren_720s_GT.jpg" 
                       title="McLaren 720S GT" 
                       description={"Leistung: 720 PS\nDrehmoment: 770 Nm\n0–100 km/h: 2,9 s\nHöchstgeschwindigkeit: 341 km/h\nAntrieb: Hinterrad\nGetriebe: 7-Gang Doppelkupplung"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Mercedes CLS AMG', 75000)} 
-                      img="assets\img\mercedes_cls.jpg" 
+                      img="assets/img/mercedes_cls.jpg" 
                       title="Mercedes CLS AMG" 
                       description={"Leistung: 612 PS\nDrehmoment: 850 Nm\n0–100 km/h: 3,7 s\nHöchstgeschwindigkeit: 250 km/h (abgeregelt)\nAntrieb: Allrad\nGetriebe: 9-Gang Automatik"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'Smart Fortwo', 15000)} 
-                      img="assets\img\smart_fortwo.jpg" 
+                      img="assets/img/smart_fortwo.jpg" 
                       title="Smart Fortwo" 
                       description={"Leistung: 82 PS\nDrehmoment: 160 Nm\n0–100 km/h: 11,6 s\nHöchstgeschwindigkeit: 130 km/h\nAntrieb: Hinterrad\nGetriebe: 1-Gang Automatik"} 
                     />
 
                     <Product 
                       onAdd={() => this.addItem(1, 'BMW i8', 60000)} 
-                      img="assets\img\BMW_i8.jpg" 
+                      img="assets/img/BMW_i8.jpg" 
                       title="BMW i8" 
                       description={"Leistung: 374 PS (kombiniert)\nDrehmoment: 570 Nm\n0–100 km/h: 4,4 s\nHöchstgeschwindigkeit: 250 km/h\nAntrieb: Allrad (Hybrid)\nGetriebe: 6-Gang Automatik"} 
                     />                  
