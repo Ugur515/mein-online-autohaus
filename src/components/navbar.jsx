997c6bb07ec736ec7cf606632ab7a95206829871
Ugur <47553873+Ugur515@users.noreply.github.com>
2025-05-25 17:49:39 +0200

    Burgermenü navbar für smartphone

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,55 +3,215 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   state = {
     showFahrzeugeDropdown: false,
+    showMobileMenu: false,
   };
 
   toggleDropdown = () => {
     this.setState(prevState => ({ showFahrzeugeDropdown: !prevState.showFahrzeugeDropdown }));
   };
 
+  toggleMobileMenu = () => {
+    this.setState(prevState => ({ showMobileMenu: !prevState.showMobileMenu }));
+  };
+
+  closeMobileMenu = () => {
+    this.setState({ showMobileMenu: false, showFahrzeugeDropdown: false });
+  };
+
   render() {
-    const { showFahrzeugeDropdown } = this.state;
+    const { showFahrzeugeDropdown, showMobileMenu } = this.state;
 
     return (
       <nav className="navbar">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Samuel AUTOHAUS</a>
-          <ul className="navbar-menu">
-            <li><a href="/">Home</a></li>
+          <a
+            className="navbar-brand"
+            href="/"
+            onClick={e => {
+              e.preventDefault();
+              window.location.reload();
+              this.closeMobileMenu();
+            }}
+          >
+            Samuel AUTOHAUS
+          </a>
 
-            <li 
+          {/* Hamburger Button sichtbar nur mobil */}
+          <button className="hamburger" onClick={this.toggleMobileMenu} aria-label="Menü umschalten">
+            ☰
+          </button>
+
+          {/* Menü: sichtbar oder versteckt je nach Bildschirm und State */}
+          <ul className={`navbar-menu ${showMobileMenu ? 'active' : ''}`}>
+            <li>
+              <a
+                href="/"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.reload();
+                  this.closeMobileMenu();
+                }}
+              >
+                Home
+              </a>
+            </li>
+
+            <li
               className="dropdown"
-              onMouseEnter={this.toggleDropdown}
-              onMouseLeave={this.toggleDropdown}
+              onMouseEnter={() => !showMobileMenu && this.toggleDropdown()}
+              onMouseLeave={() => !showMobileMenu && this.toggleDropdown()}
               style={{ position: 'relative' }}
             >
-              <a href="#!" onClick={(e) => e.preventDefault()}>
+              <a
+                href="#!"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState(prevState => ({ showFahrzeugeDropdown: !prevState.showFahrzeugeDropdown }));
+                }}
+              >
                 Fahrzeuge ▼
               </a>
               {showFahrzeugeDropdown && (
                 <ul className="dropdown-menu">
-                  <li><a href="/neuwagen">Neuwagen</a></li>
-                  <li><a href="/gebrauchtwagen">Gebrauchtwagen</a></li>
-                  <li><a href="/elektro-hybrid">Elektro & Hybrid</a></li>
+                  <li>
+                    <a
+                      href="/neuwagen"
+                      onClick={e => {
+                        e.preventDefault();
+                        window.location.href = '/neuwagen';
+                        this.closeMobileMenu();
+                      }}
+                    >
+                      Neuwagen
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/gebrauchtwagen"
+                      onClick={e => {
+                        e.preventDefault();
+                        window.location.href = '/gebrauchtwagen';
+                        this.closeMobileMenu();
+                      }}
+                    >
+                      Gebrauchtwagen
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/elektro-hybrid"
+                      onClick={e => {
+                        e.preventDefault();
+                        window.location.href = '/elektro-hybrid';
+                        this.closeMobileMenu();
+                      }}
+                    >
+                      Elektro & Hybrid
+                    </a>
+                  </li>
                 </ul>
               )}
             </li>
 
-            <li><a href="/service">Service</a></li>
-            <li><a href="/finanzierung">Finanzierung & Leasing</a></li>
-            <li><a href="/angebote">Angebote</a></li>
-            <li><a href="/ueber-uns">Über uns</a></li>
-            <li><a href="/kontakt">Kontakt</a></li>
-            <li><a href="/konto">Mein Konto</a></li>
-
-            <li className="warenkorb-icon" style={{ fontSize: '1.5rem', marginLeft: '10px', cursor: 'pointer' }}>
-            <a href="#!" title="Warenkorb" onClick={(e) => { e.preventDefault(); this.props.toggleCart(); }}>
-            🛒 {this.props.itemsCount > 0 && <span>({this.props.itemsCount})</span>}
-            </a>
+            <li>
+              <a
+                href="/service"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/service';
+                  this.closeMobileMenu();
+                }}
+              >
+                Service
+              </a>
             </li>
 
+            <li>
+              <a
+                href="/finanzierung"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/finanzierung';
+                  this.closeMobileMenu();
+                }}
+              >
+                Finanzierung & Leasing
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="/angebote"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/angebote';
+                  this.closeMobileMenu();
+                }}
+              >
+                Angebote
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="/ueber-uns"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/ueber-uns';
+                  this.closeMobileMenu();
+                }}
+              >
+                Über uns
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="/kontakt"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/kontakt';
+                  this.closeMobileMenu();
+                }}
+              >
+                Kontakt
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="/konto"
+                onClick={e => {
+                  e.preventDefault();
+                  window.location.href = '/konto';
+                  this.closeMobileMenu();
+                }}
+              >
+                Mein Konto
+              </a>
+            </li>
+
+            <li
+              className="warenkorb-icon"
+              style={{ fontSize: '1.5rem', marginLeft: '10px', cursor: 'pointer' }}
+            >
+              <a
+                href="#!"
+                title="Warenkorb"
+                onClick={e => {
+                  e.preventDefault();
+                  this.props.toggleCart();
+                  this.closeMobileMenu();
+                }}
+              >
+                🛒 {this.props.itemsCount > 0 && <span>({this.props.itemsCount})</span>}
+              </a>
+            </li>
           </ul>
         </div>
+
+        {/* Overlay, wenn Menü mobil offen ist */}
+        {showMobileMenu && <div className="overlay" onClick={this.closeMobileMenu}></div>}
       </nav>
     );
   }
